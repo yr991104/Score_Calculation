8d049a50fd972dcc10442c4ae7bd2ffe76ea8d15
yr991104 <65888031+yr991104@users.noreply.github.com>
2021-11-22 23:29:17 +0900

    과목 추가
    과목 추가 기능, 주석, 함수로 정리, 클래스 함수 추가 등등

--- main.cpp
+++ main.cpp
@@ -4,80 +4,94 @@
 #include <cassert>
 #include <cstdlib>
 #include <vector>
-//test용
+
 using namespace std;
 
-vector<string> first_semester = { "디지털논리","이산구조","고급프로그래밍","리눅스활용실습","공학수학1" };//1학기 공통과목
-vector<string> second_semester = { "객체지향프로그래밍","선형대수학","자료구조","시스템소프트웨어","자료구조실습","오픈소스소프트웨어개발" };//2학기 공통과목
-vector<string> commons;//이번 학기 공통과목 저장하는 문자열 벡터
+vector<string> first_semester = { "디지털논리","이산구조","고급프로그래밍","리눅스활용실습","공학수학1" };		//1학기 공통과목
+vector<string> second_semester = { "객체지향프로그래밍","선형대수학","자료구조","시스템소프트웨어","자료구조실습","오픈소스소프트웨어개발" };		//2학기 공통과목
+vector<string> commons;			//이번 학기 공통과목 저장하는 문자열 벡터
 
-//과목 클래스
+int semester;
+vector<string> select;		//선택한 과목 저장할 문자열 벡터
+
 class Sub {
 private:
-	string class_name;
-	string professor;
-	int students;
-	double att;
-	double mid;
-	double fin;
-	double quiz;
-	int score_mid;
-	int score_fin;
-	int score_quiz;
+	string class_name;		//과목명
+	string professor;		//담당교수님
+	int students;			//학생수
+	int att;				//성적반영비율(출석, 중간고사, 기말고사, 과제, 퀴즈)
+	int mid;
+	int fin;
+	int work;
+	int quiz;
+	int att_exp;			//기대평균치(출석, 중간고사, 기말고사, 과제, 퀴즈)
+	int mid_exp;
+	int fin_exp;
+	int work_exp;
+	int quiz_exp;
+	double score_att = 0;	//취득점수(출석, 중간고사, 기말고사, 과제, 퀴즈)
+	double score_mid = 0;	
+	double score_fin = 0;
+	double score_work = 0;
+	double score_quiz = 0;
 public:
-	void ask_subject_info();
-	string getClassName() { return this->class_name; }
-	//char getGrade();
+	void ask_subject_info(string name);						//과목 정보 입력
+
+	string getClassName() { return this->class_name; }		//정보 받아오기
+	string getProfessor() { return this->professor; }
+	int getStudent() { return this->students; }
+	int getAtt() { return this->att; }
+	int getFin() { return this->fin; }
+	int getWork() { return this->work; }
+	int getQuiz() { return this->quiz; }
+	int getAttExp() { return this->att_exp; }
+	int getMidExp() { return this->mid_exp; }
+	int getFinExp() { return this->fin_exp; }
+	int getWorkExp() { return this->work_exp; }
+	int getQuizExp() { return this->quiz_exp; }
+	double getScoreAtt() { return this->score_att; }
+	double getScoreMid() { return this->score_mid; }
+	double getScoreFin() { return this->score_fin; }
+	double getScoreWork() { return this->score_work; }
+	double getScoreQuiz() { return this->score_quiz; }
+
+	void setScoreAtt(double score) { this->score_att = score; }		//점수 입력하기
+	void setScoreMid(double score) { this->score_mid = score; }
+	void setScoreFin(double score) { this->score_fin = score; }
+	void setScoreWork(double score) { this->score_work = score; }
+	void setScoreQuiz(double score) { this->score_quiz = quiz; }
 };
-void Sub::ask_subject_info() {
 
-	cout << "과목명: ";
-	cin >> class_name;
-	cout << "담당교수 : ";
-	cin >> professor;
-	cout << "점수(중간, 기말, 과제 순): ";
-	cin >> score_mid >> score_fin >> score_quiz;
-	cout << "해당과목 수강인원: ";
-	cin >> students;
-	cout << "성적 반영 비율을 입력하세요(출석, 과제, 중간, 기말 비율 순): ";
-	cin >> att >> mid >> fin >> quiz;
-}
-/*char Sub::getGrade() {
-
-}*/
-
-
-
-void show_subject(int semester) {
+void show_subject(int semester) {		//학기 공통과목 출력
 	cout << endl;
 	cout << semester << "학기 공통과목입니다." << endl;
-	if (semester == 1)
+	if (semester == 1)			//1학기
 	{
 		for (string a : first_semester)
 		{
 			cout << a << "  ";
-			commons.push_back(a);
+			commons.push_back(a);		//공통과목 저장
 		}
 		cout << endl;
 	}
-	else
+	else						//2학기
 	{
 		for (string a : second_semester)
 		{
 			cout << a << "  ";
-			commons.push_back(a);
+			commons.push_back(a);		//공통과목 저장
 		}
 		cout << endl;
 	}
 	cout << endl;
 }
-int main(void)
+
+vector<string> subject_select(vector<string> select)		//과목 선택 함수
 {
-	vector<string> select;//선택한 과목 저장할 문자열 벡터
-	int limit = 21;//최대이수학점 21학점
-	int current_grades = 0;//현재 수강신청한 학점
-	int semester;
-restart:
+	int limit = 21;			//최대이수학점 21학점
+	int current_grades = 0;	//현재 수강신청한 학점
+
+	cout << "================================" << endl << endl;
 	cout << "2학년 몇 학기 입니까? (1/2) : ";
 	cin >> semester;
 	if (semester == 1 || semester == 2)
@@ -87,54 +101,63 @@ restart:
 	else
 	{
 		cout << "잘못된 입력입니다." << endl;
-		goto restart;
+		subject_select(select);
 	}
+
+	cout << "입력을 종료하려면 q를 입력하세요." << endl << endl;
+
 	while (current_grades <= 21)
 	{
-		string choose;
+		string choose;			//입력받는 수강 과목명
 		int cnt = 0;
-		cout << "공통과목 중 수강할 과목 선택 : " << endl;
-		cout << "과목 입력을 종료하려면 stop_sub를 입력하시오" << endl;
+		cout << "공통과목 중 수강할 과목 선택 : ";
 		cin >> choose;
-		if (choose == "stop_sub") {
+
+		if (choose == "q")
 			break;
-		}
+
 		for (string a : commons)
 		{
 			if (a == choose) {
-				cnt++;//선택한 과목이 해당학기 공통과목에 있을 시 cnt를 증가시키고 반복문을 빠져나온다.
+				cnt++;			//선택한 과목이 해당학기 공통과목에 있을 시 cnt를 증가시키고 반복문을 빠져나온다.
 				break;
 			}
 		}
-		if (cnt == 1)//cnt가 1이라는 것은 선택한 과목이 해당학기 공통과목에 존재한다는 의미이다.
+
+		if (cnt == 1)			//cnt가 1이라는 것은 선택한 과목이 해당학기 공통과목에 존재한다는 의미이다.
 		{
 			int num = 0;
+
 			for (string a : select)
 			{
 				if (a == choose) {
-					num++;//choose과 입력한 과목이 이미 select 벡터 안에 존재하는 값과 동일하면 num을 증가시키고 반복문 빠져나온다.
+					num++;		//choose과 입력한 과목이 이미 select 벡터 안에 존재하는 값과 동일하면 num을 증가시키고 반복문 빠져나온다.
 					break;
 				}
 			}
-			if (num == 1)//num이 1이라는 것은 select 벡터 안에 choose와 같은 값이 존재하는 것이다.
+
+			if (num == 1)	//num이 1이라는 것은 select 벡터 안에 choose와 같은 값이 존재하는 것이다.
 			{
 				cout << "해당과목을 이미 신청하셨습니다.";
 				cout << endl;
 			}
-			else//num이 1이 아닌 경우 즉 select 벡터 안에 choose와 동일한 값이 없다는 것이다. 
+
+			else			//num이 1이 아닌 경우 즉 select 벡터 안에 choose와 동일한 값이 없다는 것이다. 
 			{
-				select.push_back(choose);//select안에 choose를 추가시킽다.
-				if (choose.find("실습") != string::npos)//실습과목인 경우 현재 신청한 학점이 2점 추가된다.
+				select.push_back(choose);					//select안에 choose를 추가시킨다.
+
+				if (choose.find("실습") != string::npos)		//실습과목인 경우 현재 신청한 학점이 2점 추가된다.
 				{
 					current_grades += 2;
 				}
-				else//실습과목이 아닌 경우 현재 신청한 학점이 3점 추가된다.
+				else		//실습과목이 아닌 경우 현재 신청한 학점이 3점 추가된다.
 				{
 					current_grades += 3;
 				}
 			}
 		}
-		else//cnt가 1이 아닌 경우 즉 0 인경우 선택한 과목이 해당 학기 공통과목에 없다는 것을 의미한다. 
+
+		else			//cnt가 1이 아닌 경우 즉 0 인경우 선택한 과목이 해당 학기 공통과목에 없다는 것을 의미한다. 
 		{
 			cout << "해당과목은 이번학기 공통과목에 없습니다.";
 			cout << endl;
@@ -150,44 +173,153 @@ restart:
 		cout << endl;
 	}
 
+	return select;
+}
 
+void print_subject(vector<string> select)		//선택 과목 출력
+{
+	cout << endl;
+	cout << "=============현재 수강중인 과목=============" << endl<<endl;
+	for (string a : select)
+		cout << a << " ";
+	cout << endl << endl << "=============================================" << endl;
+}
 
+void Sub::ask_subject_info(string name) {			//class의 변수를 이용한 과목 정보 입력
+	cout << "과목명 : " << name << endl;
+	class_name = name;
+	cout << "담당교수 : ";
+	cin >> professor;
+	cout << "해당과목 수강인원 : ";
+	cin >> students;
+	cout << "성적 반영 비율 (출석, 중간, 기말, 과제, 퀴즈 비율 순) : ";
+	cin >> att >> mid >> fin >> work >> quiz;
+	cout << "기대 평균치 (출석, 중간, 기말, 과제, 퀴즈 비율 순) : ";
+	cin >> att_exp >> mid_exp >> fin_exp >> work_exp >> quiz_exp;
+}
 
-	vector<Sub> sub_vec;
-	vector<Sub>::iterator iter;
-
+vector<Sub> input_subject_info(vector<string> select, vector<Sub> sub_vec)			//수강중인 과목 전체 정보 입력 호출
+{
 	for (int i = 0; i < select.size(); i++) {
 		Sub s;
-		cout << " 성적 산출을 위한 정보를 입력해주세요" << endl;
-		s.ask_subject_info();
+		cout << endl << "성적 산출을 위한 정보를 입력해주세요" << endl;
+		s.ask_subject_info(select.at(i));		//과목을 지정하여 정보 입력 함수 호출
 		cout << endl;
 		sub_vec.push_back(s);
-
 	}
 
-	cout << "성적입력이 완료되었습니다. 원하시는 기능을 선택해 주세요" << endl;
-	cout << "1. 해당과목 학점 계산" << endl;
-	cout << "2. 전체 학점 계산" << endl;
-	cout << "3. 프로그램 종료" << endl;
+	return sub_vec;			//sub_vec를 리턴하여 과목 정보 리턴
+}
+
+vector<Sub> add_subject(vector<string> select, vector<Sub> sub_vec)			//과목 추가 함수
+{
+	string name;
+	Sub s;
+
+	cout << endl << "과목명을 입력하세요 : ";
+	cin >> name;
+
+	s.ask_subject_info(name);		//입력받은 과목명의 과목 정보
+	sub_vec.push_back(s);			//sub_vec에 과목 정보 추가
+
+	cout << endl << "입력되었습니다." << endl;
+
+	return sub_vec;			//sub_vec를 리턴하여 과목 정보 리턴
+}
+
+vector<Sub> input_subject_score(vector<Sub> sub_vec, string name)		//취득 점수 입력
+{
+	vector<Sub>::iterator iter;
+	double score;
+
+	for (iter = sub_vec.begin(); iter != sub_vec.end(); iter++)
+	{
+		if (iter->getClassName() == name)			//과목명을 class에서 찾기
+		{
+			cout << endl << "출석 점수 : ";			//점수 입력받기 및 설정하기
+			cin >> score;
+			iter->setScoreAtt(score);
+			cout << "중간고사 점수 : ";
+			cin >> score;
+			iter->setScoreMid(score);
+			cout << "기말고사 점수 : ";
+			cin >> score;
+			iter->setScoreFin(score);
+			cout << "과제 점수 : ";
+			cin >> score;
+			iter->setScoreWork(score);
+			cout << "퀴즈 점수 : ";
+			cin >> score;
+			iter->setScoreQuiz(score);
+		}
+	}
+
+	return sub_vec;
+}
+
+string get_subject_score(vector<Sub> sub_vec, string name)		//입력받은 취득 점수로 학점 계산
+{
+
+}
+
+int main(void)
+{
+	vector<Sub>::iterator iter;
+	vector<Sub> sub_vec;
+
+	select = subject_select(select);			//과목 선택
+
+	print_subject(select);						//선택한 과목 출력
+
+	sub_vec = input_subject_info(select , sub_vec);			//과목 정보 입력
+	
+	cout << "==================================" << endl;
+	cout << "성적입력이 완료되었습니다." << endl;
+	cout << "1. 과목 학점 계산" << endl;
+	cout << "2. 과목 추가" << endl;
+	cout << "3. 수강중인 과목 조회" << endl;
+	cout << "4. 프로그램 종료" << endl;
+	cout << "==================================" << endl << endl;
+	cout << "원하시는 기능을 선택해 주세요 : ";
+
 	int usrc;
 	cin >> usrc;
 	string usrchoice;
-	switch (usrc)
+	
+	while (usrc != 3)
 	{
-	case 1:
-		cout << "무슨 과목의 학점을 계산하길 원하시나요?: ";
-		cin >> usrchoice;
-		for (iter = sub_vec.begin(); iter != sub_vec.end(); iter++) {
-			if (iter->getClassName() == usrchoice) {
-				cout << "해당과목의 예상 학점은 " << "A" << "입니다" << endl;
+		int a = 0;
+
+		switch (usrc)
+		{
+		case 1:
+			cout << endl << "과목명을 입력하세요: ";
+			cin >> usrchoice;
+			for (iter = sub_vec.begin(); iter != sub_vec.end(); iter++) {			//sub_vec의 시작부터 끝까지 탐색
+				if (iter->getClassName() == usrchoice) {					//입력받은 과목명과 같은 경우
+					input_subject_score(sub_vec, usrchoice);				//취득점수 입력받기
+					string score = get_subject_score(sub_vec, usrchoice);					//학점 구하기
+					cout << endl << "해당과목의 예상 학점은 " << score << "입니다" << endl;
+					a = 1;
+				}
 			}
+			if(a != 1)			//과목명이 없는 경우
+				cout << "수강하지 않은 과목입니다." << endl;
+			break;
+		case 2:
+			sub_vec = add_subject(select, sub_vec);					//과목 추가
+			select.push_back(sub_vec.back().getClassName());		//select에도 과목명 추가
+			break;
+		case 3:
+			print_subject(select);
+			break;
 		}
-		break;
-	case 3:
-		break;
-	default:
-		break;
+
+		cout << endl << "==================================" << endl;
+		cout << "원하시는 기능을 선택해 주세요 : ";
+		cin >> usrc;
 	}
+
 	return 0;
 
 }
